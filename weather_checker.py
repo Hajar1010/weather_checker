f7de79f05fc4a9291684750124e0e57f98e89eba
Hajar <hajaressalihi9@gmail.com>
2025-08-07 02:32:34 +0100

    Update weather_checker.py

--- weather_checker.py
+++ weather_checker.py
@@ -2,21 +2,27 @@
 # Author: hajar1010
 # Description: A command-line app that shows the current weather for a given city.
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "94244daaab1b75442593070b0e912584"
+load_dotenv() 
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
 while True:
-    city = input("Enter your city : ")
+    city = input("Enter your city: ")
     if city == "":
         break
 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
+
     if response.status_code == 200:
         print("Request successful!")
         data = response.json()
         temperature = data["main"]["temp"]
         humidity = data["main"]["humidity"]
         condition = data["weather"][0]["description"]
+
         print(f"\nWeather in {city}:")
         print(f"Temperature: {temperature}°C")
         print(f"Condition: {condition.capitalize()}")
